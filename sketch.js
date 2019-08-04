86871390acc48a870e7f5f405679a88a450406bd
dennisfreitag <dennisfreitag@gmail.com>
2019-08-04 20:25:17 +0200

    setting up for 2d rendering

--- sketch.js
+++ sketch.js
@@ -2,23 +2,26 @@ let walls = []
 let ray
 let particle
 let xoff = 0
-let yoff = 1000
+let yoff = 10000
+
+const sceneW = 400
+const sceneH = 400
 
 function setup() {
-  createCanvas(400, 400)
+  createCanvas(800, 400)
 
   for (let i = 0; i < 5; i++) {
-    let x1 = random(width)
-    let x2 = random(width)
-    let y1 = random(height)
-    let y2 = random(height)
+    let x1 = random(sceneW)
+    let x2 = random(sceneW)
+    let y1 = random(sceneH)
+    let y2 = random(sceneH)
     walls[i] = new Boundary(x1, y1, x2, y2)
   }
 
-  walls.push(new Boundary(0, 0, width, 0))
-  walls.push(new Boundary(width, 0, width, height))
-  walls.push(new Boundary(width, height, 0, height))
-  walls.push(new Boundary(0, height, 0, 0))
+  walls.push(new Boundary(0, 0, sceneW, 0))
+  walls.push(new Boundary(sceneW, 0, sceneW, sceneH))
+  walls.push(new Boundary(sceneW, sceneH, 0, sceneH))
+  walls.push(new Boundary(0, sceneH, 0, 0))
 
   particle = new Particle()
 
@@ -30,8 +33,8 @@ function draw() {
   for (let wall of walls) {
     wall.show()
   }
-    particle.update(noise(xoff) * width, noise(yoff) * height)
-    particle.show()
+  particle.update(noise(xoff) * sceneW, noise(yoff) * sceneH)
+  particle.show()
   particle.look(walls)
   
   xoff += 0.01
