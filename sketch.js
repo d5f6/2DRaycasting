1411a714308bd698d6e696225f2fc5f7fdd61eff
dennisfreitag <dennisfreitag@gmail.com>
2019-08-04 19:49:20 +0200

    auto movement with edges

--- sketch.js
+++ sketch.js
@@ -15,7 +15,11 @@ function setup() {
     walls[i] = new Boundary(x1, y1, x2, y2)
   }
 
-  ray = new Ray(100, 200)
+  walls.push(new Boundary(0, 0, width, 0))
+  walls.push(new Boundary(width, 0, width, height))
+  walls.push(new Boundary(width, height, 0, height))
+  walls.push(new Boundary(0, height, 0, 0))
+
   particle = new Particle()
 
 }
